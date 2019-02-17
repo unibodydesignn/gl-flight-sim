fd06ecdd3b1b3fef91476a54beca9a53d00d7b23
unibodydesignn <unibodydesignn@gmail.com>
2019-02-17 15:57:17 +0300

    GLM has been added.

---
+++ gl-modeling-tool/Cube.h
@@ -0,0 +1,22 @@
+//
+//  Cube.h
+//  gl-modeling-tool
+//
+//  Created by Melih Mete on 17.02.2019.
+//  Copyright © 2019 Melih Mete. All rights reserved.
+//
+
+#ifndef Cube_h
+#define Cube_h
+
+class Cube {
+
+public:
+    Cube() { length = 5; }
+    ~Cube() { }
+    
+private:
+    int length;
+    
+};
+#endif /* Cube_h */
--- gl-modeling-tool/MainWindow.h
+++ gl-modeling-tool/MainWindow.h
@@ -11,8 +11,13 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <iostream>
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm/vec3.hpp>
+#include "Cube.h"
+
 
 class MainWindow {
 
@@ -22,7 +27,15 @@ public:
         windowsWidth  = 1920;
     }
     ~MainWindow() { }
-
+    
+    static void cursorPositionCallback(GLFWwindow *window, double xPos, double yPos) {
+        std::cout << "XPOS : " << xPos << " ypos : " << yPos << std::endl;
+    }
+    
+    void cursorEnterCallback(GLFWwindow *window, int entered) {
+        
+    }
+    
     void Init() {
         if(glfwInit()) {
             std::cout << "Could not initiate GLFW" << std::endl; }
@@ -34,6 +47,9 @@ public:
             
         }
         
+        glfwSetCursorPosCallback(window, cursorPositionCallback);
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+        glfwGetFramebufferSize(window, &windowsWidth, &windowsHeigth);
         glfwMakeContextCurrent( window );
         glViewport(0.0f, 0.0f, windowsWidth, windowsHeigth);
         glMatrixMode(GL_PROJECTION);
@@ -47,7 +63,7 @@ public:
         // Loop until the user closes the window
         while ( !glfwWindowShouldClose( window) )
         {
-            glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+            glClear(GL_COLOR_BUFFER_BIT |GL_DEPTH_BUFFER_BIT);
             glClearColor(0.0f/255.0f, 129.0f/255.0f, 215.0f/255.0f, 1.0f);
             glfwSwapBuffers(window);
             
