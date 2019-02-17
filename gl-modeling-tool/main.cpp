d72a638803fa49504c82c480b986dcca5f721544
unibodydesignn <unibodydesignn@gmail.com>
2019-02-17 12:44:36 +0300

    First initial commit

---
+++ gl-modeling-tool/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  gl-modeling-tool
+//
+//  Created by Melih Mete on 17.02.2019.
+//  Copyright © 2019 Melih Mete. All rights reserved.
+//
+
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+
+int main(int argc, const char * argv[]) {
+    
+    std::cout << "Merhaba dunya " << std::endl;
+    return 0;
+}
